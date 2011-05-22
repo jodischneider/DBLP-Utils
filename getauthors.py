ebae5ad728ab06799e21e007a5021dca6db98699
Jodi Schneider <jodi.a.schneider@gmail.com>
2011-05-22 23:40:13 +0100

    deduplicate, sort, translate to Unicode; add DEBUG flag and help text

--- getauthors.py
+++ getauthors.py
@@ -1,10 +1,16 @@
+'''Grabs the list of authors from a DBLP entry. Outputs Unicode authors.txt with
+the deduplicated list of authors, one author per line.'''
+
 import urllib
 import codecs
 from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulStoneSoup
+
+#Debugging flag
+DEBUG = False
 
 authors = []
 
-#f = urllib.urlopen("http://www.informatik.uni-trier.de/~ley/db/journals/ws/ws6.html")
 f = urllib.urlopen("http://www.informatik.uni-trier.de/~ley/db/conf/www/www2010.html")
 s = f.read()
 f.close()
@@ -13,13 +19,25 @@ allli = soup.findAll( "li")
 for li in allli:
    alla = li.findAll('a')
    for a in alla:
-     #reenable for testing
-     #print a
      author = a.text
      if author:
-       authors.append(author+"\r")
+       #convert HTML entities
+       #via StackOverflow http://stackoverflow.com/questions/701704/how-do-you-convert-html-entities-to-unicode-and-vice-versa-in-python
+       author = unicode(BeautifulSoup(author, convertEntities=BeautifulSoup.HTML_ENTITIES))
+       authors.append(author)
 
+#deduplicate
+authorset = frozenset(authors)
+
+#sort
+deduped_authors = sorted(list(authorset))
+
+#output deduplicated list to a file
 outfile = codecs.open("authors.txt", "w", "utf-8")
-for author in authors:
- outfile.write(author)
+
+for author in deduped_authors:
+ if DEBUG:
+ 	print author
+ outfile.write(author+"\r")
+
 outfile.close()
